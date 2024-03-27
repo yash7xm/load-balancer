177c039e267cf112199586692f59a994bfc5f071
yash7xm <yash.7xm@gmail.com>
2024-03-27 23:07:30 +0530

    stucts added

--- main.go
+++ main.go
@@ -1,6 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http/httputil"
+	"net/url"
+	"sync"
+)
+
+type Backend struct {
+	URL          *url.URL
+	Alive        bool
+	mux          sync.RWMutex
+	ReverseProxy *httputil.ReverseProxy
+}
+
+type ServerPool struct {
+	backends []*Backend
+	current  uint64
+}
 
 func main() {
 	fmt.Println("A simple load-balancer implementiaiton in golang")
