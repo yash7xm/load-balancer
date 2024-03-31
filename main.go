b6d347501530ee0d0811ff565d8d3a70a7ee987d
yash7xm <yash.7xm@gmail.com>
2024-04-01 00:16:41 +0530

    backedns add to server pool

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"strings"
 	"sync"
 )
 
@@ -22,14 +23,45 @@ type ServerPool struct {
 	current  uint64
 }
 
+func (s *ServerPool) AddBackend(backend *Backend) {
+	s.backends = append(s.backends, backend)
+}
+
+var serverPool ServerPool
+
 func main() {
+	var serverList string
 	var port int
+	flag.StringVar(&serverList, "backends", "", "Load balanced backends, use comma to seprate")
 	flag.IntVar(&port, "port", 3030, "Port to Serve")
 	flag.Parse()
+
+	if len(serverList) == 0 {
+		log.Fatal("Please provide one or more backends to load balance")
+	}
+
+	//parse servers
+	tokens := strings.Split(serverList, ",")
+	for _, tok := range tokens {
+		serverUrl, err := url.Parse(tok)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		proxy := httputil.NewSingleHostReverseProxy(serverUrl)
+
+		serverPool.AddBackend(&Backend{
+			URL:          serverUrl,
+			Alive:        true,
+			ReverseProxy: proxy,
+		})
+	}
 
 	server := http.Server{
 		Addr: fmt.Sprintf(":%d", port),
 	}
+
+	log.Printf("Server Pool %v\n", serverPool.backends)
 
 	log.Printf("Load Balancer started at :%d\n", port)
 	if err := server.ListenAndServe(); err != nil {
