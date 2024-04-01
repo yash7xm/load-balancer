008b8b99f002aa2ec588b68e7e9128a64d04ba2b
yash7xm <yash.7xm@gmail.com>
2024-04-01 17:10:19 +0530

    getiing next alive backend added

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"net/url"
 	"strings"
 	"sync"
+	"sync/atomic"
 )
 
 type Backend struct {
@@ -18,6 +19,21 @@ type Backend struct {
 	ReverseProxy *httputil.ReverseProxy
 }
 
+// set alive for this backend
+func (b *Backend) SetAlive(alive bool) {
+	b.mux.Lock()
+	b.Alive = true
+	b.mux.Unlock()
+}
+
+// is alive returns true when the backend is alive
+func (b *Backend) IsAlive() (alive bool) {
+	b.mux.RLock()
+	alive = b.Alive
+	b.mux.RUnlock()
+	return
+}
+
 type ServerPool struct {
 	backends []*Backend
 	current  uint64
@@ -25,6 +41,27 @@ type ServerPool struct {
 
 func (s *ServerPool) AddBackend(backend *Backend) {
 	s.backends = append(s.backends, backend)
+}
+
+// Next Index atomically increase the counter and returns the index
+func (s *ServerPool) NextIndex() int {
+	return int(atomic.AddUint64(&s.current, uint64(1)) % uint64(len(s.backends)))
+}
+
+func (s *ServerPool) GetNextPeer() *Backend {
+	next := s.NextIndex()
+	l := len(s.backends) + next
+	for i := next; i < l; i++ {
+		idx := i % len(s.backends)
+		if s.backends[idx].IsAlive() {
+			if i != next {
+				atomic.StoreUint64(&s.current, uint64(idx))
+			}
+			return s.backends[idx]
+		}
+	}
+
+	return nil
 }
 
 var serverPool ServerPool
@@ -55,15 +92,15 @@ func main() {
 			Alive:        true,
 			ReverseProxy: proxy,
 		})
+
+		log.Printf("Configured server: %s\n", serverUrl)
 	}
 
 	server := http.Server{
 		Addr: fmt.Sprintf(":%d", port),
 	}
 
-	log.Printf("Server Pool %v\n", serverPool.backends)
-
-	log.Printf("Load Balancer started at :%d\n", port)
+	log.Printf("Load Balancer started at http://localhost:%d\n", port)
 	if err := server.ListenAndServe(); err != nil {
 		log.Fatal(err)
 	}
